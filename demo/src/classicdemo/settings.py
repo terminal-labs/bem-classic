174963955c417533b50b910b339fd1be70617bb0
Michael Verhulst <michael@terminallabs.com>
2023-12-09 17:55:27 -0600

    cleaning

--- demo/src/classicdemo/settings.py
+++
@@ -1,7 +0,0 @@
-import os.path
-from importlib.metadata import version
-
-PROJECT_ROOT = os.path.realpath(os.path.dirname(__file__))
-PROJECT_NAME = os.path.basename(PROJECT_ROOT)
-PROJECTNAME = PROJECT_NAME.replace("_", "").replace("-", "")
-VERSION = version(PROJECT_NAME)
