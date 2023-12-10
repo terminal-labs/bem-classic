34d38c83d7f9ca374ed286e04c93f9ca1d6b84a4
Michael Verhulst <michael@terminallabs.com>
2023-12-09 18:00:33 -0600

    Create launcher.sh

---
+++ demo/launcher.sh
@@ -0,0 +1,10 @@
+apt update
+apt install build-essential -y
+apt install linux-headers-$(uname -r) -y
+apt install make -y
+apt install unzip -y
+wget https://github.com/terminal-labs/bem-classic/archive/refs/heads/main.zip
+unzip main.zip
+chmod -R 7777 bem-classic
+cd bem-classic/demo
+make psf
